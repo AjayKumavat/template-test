365a500fa1c0b19af999363ed0e8fd30318f5113
AjayKumavat <akmvt0@gmail.com>
2021-06-09 15:55:56 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+// The browser windows is called as a window and inside that window there's a console where we can log something and show it in the browser
+// window.console.log Here, we don't need to write window because by default all javascript is in the window element
+// console.log(testing);
